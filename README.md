6d7f8145a540bd20b2ee85a95dc5773bf71c0f80
Evgenii Ryshkov <eryshkov@gmail.com>
2019-02-19 11:19:59 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,27 @@
+#  Home work 9
+
+<div align = "center">
+<img src="/screens/hw09.png" width="100%">  
+<br>
+</div>
+
+<p align="center">
+<img src="https://img.shields.io/badge/PHP-7.2-orange.svg" alt="PHP-7.2"/>
+<img src="https://img.shields.io/badge/licence-MIT-lightgray.svg" alt="Licence MIT"/>
+</p>
+
+## Информация для проверяющего
+* 
+
+## Main functionality
+* uses MySQL database
+* classes and objects
+* view separation
+* uses output buffer
+
+## Credits
+* thanks to **Albert Stepantsev** and to his [awesome school](https://pr-of-it.ru/courses/php-1.html)
+
+## License
+
+This project is licensed under the MIT License.
---
+++ index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
+          integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"
+            integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k"
+            crossorigin="anonymous"></script>
+
+    <title>HomeWork 8</title>
+</head>
+<body>
+<p></p>
+<div class="container">
+    <a href="/test.php" class="btn btn-primary">Test</a>
+    <a href="/03_news/news.php" class="btn btn-primary">News</a>
+
+</div>
+</body>
+</html>
