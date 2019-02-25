d75f006ab3e1618b060976a33e1aa414db0cd487
Evgenii Ryshkov <eryshkov@gmail.com>
2019-02-25 11:47:55 +0300

    db dump is updated

--- db_dump/hw09_blocks.sql
+++ db_dump/hw09_blocks.sql
@@ -1,2 +1,4 @@
 INSERT INTO hw09.blocks (id, blockName, pageName, isHidden, blockText, imageName, `order`) VALUES (1, 'text', 'about', 0, 'Text about me', null, 1);
-INSERT INTO hw09.blocks (id, blockName, pageName, isHidden, blockText, imageName, `order`) VALUES (2, 'image', null, null, null, null, 1);
+INSERT INTO hw09.blocks (id, blockName, pageName, isHidden, blockText, imageName, `order`) VALUES (2, 'image', 'gallery', 0, null, 'image_1s.jpg', 1);
+INSERT INTO hw09.blocks (id, blockName, pageName, isHidden, blockText, imageName, `order`) VALUES (3, 'image', 'gallery', 0, null, 'image_2s.jpg', 2);
+INSERT INTO hw09.blocks (id, blockName, pageName, isHidden, blockText, imageName, `order`) VALUES (4, 'image', 'gallery', 0, null, 'image_3s.jpg', 3);
