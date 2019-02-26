986c66f49c0c0879b671f3b69b29194f9d43b907
Evgenii Ryshkov <eryshkov@gmail.com>
2019-02-27 00:20:08 +0300

    addRecord.php is added

--- addRecord.php
+++ addRecord.php
@@ -1,2 +1,14 @@
 <?php
+require_once __DIR__ . '/autoload.php';
 
+$myGuestBook = new \Model\GuestBook();
+
+if (isset($_POST['message'])) {
+    //Применил возврат $this из метода, что дает возможность сразу вызвать следующий метод
+    $myGuestBook->append($_POST['message'])->save();
+
+    header('Location: /index.php?page=guestbook/');
+    exit;
+} else {
+    http_response_code(400);
+}
