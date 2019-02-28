21f1ce78a38642604e80135099cc08d94150ccd0
Evgenii Ryshkov <eryshkov@gmail.com>
2019-02-28 11:37:21 +0300

    Menu.php is implemented

--- classes/Model/Menu.php
+++ classes/Model/Menu.php
@@ -4,11 +4,21 @@ namespace Model;
 
 class Menu
 {
-
-    public function getVisible($onPage)
+    /**
+     * @param string $onPage
+     * @return MenuItem[]
+     */
+    public function getVisible(string $onPage): array
     {
         $db = new DB();
         $sql = 'SELECT * FROM pages WHERE isHidden=FALSE ORDER BY `order`';
         $menuItems = $db->query($sql, []);
+
+        $result = [];
+        foreach ($menuItems as $item) {
+            $result[] = new MenuItem($item['pageName'], $item['displayName'], $item['order'], $onPage === $item['pageName']);
+        }
+
+        return $result;
     }
 }
--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 
 require_once __DIR__ . '/autoload.php';
 
-
+$menu = new \Model\Menu();
+$menuItems = $menu->getVisible('about');
 
 $view = new \View\View();
 $view->assign('currentPage', $currentPage);
