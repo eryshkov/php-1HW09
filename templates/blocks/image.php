d10395a6a3ded26a67c7242c9a80fcb9abfc41af
Evgenii Ryshkov <eryshkov@gmail.com>
2019-02-28 10:58:55 +0300

    clean all

--- templates/blocks/image.php
+++
@@ -1,5 +0,0 @@
-<div class="row">
-    <div class="col">
-        <img src="/img/<?php echo $imageName; ?>">
-    </div>
-</div>
--- templates/blocks/text.php
+++
@@ -1,7 +0,0 @@
-<div class="row">
-    <div class="col">
-        <?php
-        echo $blockText;
-        ?>
-    </div>
-</div>
