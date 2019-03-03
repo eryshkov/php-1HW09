879a8f0ab8df4d86bab566bbcb11fa550fc81a20
Evgenii Ryshkov <eryshkov@gmail.com>
2019-03-03 10:33:22 +0300

    updateAboutText.php is fixed

--- updateAboutText.php
+++ updateAboutText.php
@@ -14,12 +14,18 @@ if (null === $currentUser) {
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
     $aboutTextBlocks = new \Model\AboutText();
-    $result = $aboutTextBlocks->getTextBlockWith($id);
-
-    if (isset($result)) {
-        //show edit form
-    }
+    $textBlock = $aboutTextBlocks->getTextBlockWith($id);
+} else {
+    header('Location:' . '/index.php?edited=0');
+    exit();
 }
 
-header('Location:' . '/index.php?edited=0');
-exit();
+$menu = new \Model\Menu();
+$menuItems = $menu->getVisibleItems('index');
+
+$view = new \View\View();
+$view->assign('menuItems', $menuItems);
+$view->assign('textBlock', $textBlock);
+$view->assign('currentUser', $currentUser);
+$view->display(__DIR__ . '/templates/updateAbout.php');
+
